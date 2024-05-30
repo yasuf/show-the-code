9aaf645db046b3e2c257e3658de34fc9b6b536ed
Yasu Flores <yasu@fountain.com>
2024-05-29 21:15:45 -0500

    hide sidebar section

--- next_design_yasu/client/src/pages/index.tsx
+++ next_design_yasu/client/src/pages/index.tsx
@@ -4,8 +4,7 @@ import Editor from '../components/Editor';
 import Accordion from '../components/Accordion';
 
 const Home: NextPage = () => {
-  // const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set(["1"]));
-  const accordionItems = [                                                                                                                                                                                                     
+  const accordionItems = [
     {
       title: 'Documentation',
       content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
@@ -24,27 +23,22 @@ const Home: NextPage = () => {
     },
   ];
     return (
-      <div className="quadrant-bg bg-[#FFFFFF] w-full gap-4">
-        <article className='col-start-2 col-span-2 text-pretty'>
-          <h1 className='articleFont text-center font-[700] text-[40px] leading-[3.19rem] text-[#000000] pt-[57px]'>an</h1>
-          <p className='articleFont font-[inter] text-[16px] text-center leading-[1.21rem] font-[300] text-[#646464] pt-[20px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <br />industry's standard dummy text ever since the 1500s</p>
-        </article>
-
+      <div>
         <div className="mx-auto flex justify-center min-h-screen w-full main_tool">
           <div className="... h-[33.209rem]">
             <Editor />
           </div>
 
-          <div className='...'>
+          {/* <div className='...'>
             <div className="flex flex-1 my-[19.67px]">
                 <div className="flex accordionFont items-center justify-center ml-[61.79px] w-[540px]">
-                  {/* <Accordion 
-                    items={accordionItems} 
-                  /> */}
+                  <Accordion
+                    items={accordionItems}
+                  />
                   <Accordion />
                 </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     );
